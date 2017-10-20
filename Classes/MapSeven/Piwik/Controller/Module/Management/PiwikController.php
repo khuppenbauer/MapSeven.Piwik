1f86840580fc039067bc96be277c21a5b7ed0aa5
Kerstin Huppenbauer <k.huppenbauer@web.de>
2017-10-20 22:35:41 +0200

    TASK: Apply migration Neos.Flow-20161124204701
    Adjusts code to package renaming from "Neos.Flow.Utility.Files" to
    "Neos.Utility.Files" and other extractions of the "Utility" packages.

--- Classes/MapSeven/Piwik/Controller/Module/Management/PiwikController.php
+++ Classes/MapSeven/Piwik/Controller/Module/Management/PiwikController.php
@@ -10,7 +10,7 @@ use Neos\Flow\Annotations as Flow;
 use Neos\Flow\Configuration\ConfigurationManager;
 use Neos\Flow\Configuration\Source\YamlSource;
 use Neos\Flow\Package\PackageManagerInterface;
-use Neos\Flow\Utility\Arrays;
+use Neos\Utility\Arrays;
 use Neos\Flow\Http\Client\Browser;
 use Neos\Flow\Http\Client\CurlEngine;
 use TYPO3\Neos\Controller\Module\AbstractModuleController;
